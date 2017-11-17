2e54a370f4e21038e29d2ab3efd4b60290d5c9f1
Ray-HP\Ray <taya-sk11@mail.ru>
2017-11-18 00:33:57 +0100

    Before merging StartScene.

--- Assets/Scripts/Singletons/StartGame.cs
+++ Assets/Scripts/Singletons/StartGame.cs
@@ -5,27 +5,46 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
+
+    public GameObject Credits;
+    public GameObject HeckerInfo;
+    public GameObject PoliceInfo;
+    public GameObject CitizenInfo;
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Credits.gameObject.activeSelf || HeckerInfo.gameObject.activeSelf || PoliceInfo.gameObject.activeSelf || CitizenInfo.gameObject.activeSelf)
         {
-            Application.Quit();
+            if ((Input.anyKeyDown) || (Input.GetMouseButton(1)))
+            {
+                Credits.gameObject.SetActive(false);
+                HeckerInfo.gameObject.SetActive(false);
+                PoliceInfo.gameObject.SetActive(false);
+                CitizenInfo.gameObject.SetActive(false);
+            }
         }
-        if (Input.GetKey(KeyCode.Keypad1) || (Input.GetKey(KeyCode.Alpha1)))
+        else
         {
-            LoadTutorial();
-        }
-        else if (Input.GetKey(KeyCode.Keypad2) || (Input.GetKey(KeyCode.Alpha2)))
-        {
-            LoadCity();
-        }
-        else if (Input.GetKey(KeyCode.Keypad3) || (Input.GetKey(KeyCode.Alpha3)))
-        {
-            LoadLargeMap();
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+            if (Input.GetKey(KeyCode.Keypad1) || (Input.GetKey(KeyCode.Alpha1)))
+            {
+                LoadTutorial();
+            }
+            else if (Input.GetKey(KeyCode.Keypad2) || (Input.GetKey(KeyCode.Alpha2)))
+            {
+                LoadCity();
+            }
+            else if (Input.GetKey(KeyCode.Keypad3) || (Input.GetKey(KeyCode.Alpha3)))
+            {
+                LoadLargeMap();
+            }
         }
     }
 
@@ -50,4 +69,28 @@ public class StartGame : MonoBehaviour {
 		Debug.Log("LoadCity()");
 		SceneManager.LoadScene ("CityScene");
 	}
+
+    public void ShowCredits()
+    {
+        AkSoundEngine.PostEvent("Play_buttonclick", gameObject);
+        Credits.gameObject.SetActive(true);
+    }
+
+    public void ShowPlayerPicture()
+    {
+        AkSoundEngine.PostEvent("Play_buttonclick", gameObject);
+        HeckerInfo.gameObject.SetActive(true);
+    }
+
+    public void ShowPolicePicture()
+    {
+        AkSoundEngine.PostEvent("Play_buttonclick", gameObject);
+        PoliceInfo.gameObject.SetActive(true);
+    }
+
+    public void ShowCitizenPicture()
+    {
+        AkSoundEngine.PostEvent("Play_buttonclick", gameObject);
+        CitizenInfo.gameObject.SetActive(true);
+    }
 }
